7e18aedf04c6398071d3738b4cd97eaba5f7d33f
ChrisZyy3 <48314211+ChrisZyy3@users.noreply.github.com>
2025-09-14 12:25:52 +0800

    feat: add shared theme tokens and wrap layout with config providers

---
+++ lib/theme.ts
@@ -0,0 +1,6 @@
+export const themeTokens = {
+  colorPrimary: '#00b96b',
+  borderRadius: 4,
+};
+export type ThemeTokens = typeof themeTokens;
+export default themeTokens;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import { ConfigProvider as AntdConfigProvider } from 'antd';
+import { ConfigProvider as AntdMobileConfigProvider } from 'antd-mobile';
+import themeTokens from '../lib/theme';
 import 'antd/dist/reset.css';
+import 'antd-mobile/es/global';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AntdConfigProvider theme={{ token: themeTokens }}>
+      <AntdMobileConfigProvider
+        theme={{
+          '--adm-color-primary': themeTokens.colorPrimary,
+          '--adm-border-radius': `${themeTokens.borderRadius}px`,
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AntdMobileConfigProvider>
+    </AntdConfigProvider>
   );
 }
