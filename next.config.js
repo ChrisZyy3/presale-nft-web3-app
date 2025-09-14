6d028e3adcce33a55fb88e389976552a474fa586
ChrisZyy <412896907@qq.com>
2025-09-14 12:04:02 +0800

    feat: 正常运行

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['antd-mobile'],
+  experimental: {
+    esmExternals: 'loose'
+  }
+}
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import 'antd/dist/reset.css';
-import 'antd-mobile/es/global';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
